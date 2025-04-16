13c69e9a39e7790539074a21f8060881d86cd7bf
CJ Pais <cj@cjpais.com>
2025-04-16 12:53:01 -0700

    ?

--- app/components/Document.tsx
+++ app/components/Document.tsx
@@ -31,7 +31,7 @@ const Document = ({ children, title }: { children: Child; title: string }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
         {import.meta.env.PROD ? (
-          <link href="/static/style.css" />
+          <link href="/static/style.css" rel="stylesheet" />
         ) : (
           <Link href="/app/style.css" rel="stylesheet" />
         )}
