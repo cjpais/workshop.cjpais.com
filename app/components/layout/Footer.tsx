8bdd870e36e8b7c4a65d589890e2d2de7157ae9a
CJ Pais <cj@cjpais.com>
2025-08-01 21:44:11 -0700

    Update Footer.tsx

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -4,7 +4,9 @@ import { HeartIcon } from "../icons/HeartIcon";
 const Footer = () => {
   return (
     <footer className="flex w-full justify-center self-center items-center mt-6 sm:mt-12 pt-4 border-t border-very-dim text-sm text-center flex-shrink-0">
-      <CJIcon />
+      <a href="https://cjpais.com" target="_blank">
+        <CJIcon className="hover:fill-accent" />
+      </a>
       with <HeartIcon />
     </footer>
   );
